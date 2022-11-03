7b364fff34cb691ba8f6d4c0b6f1c91f2f5b346b
wmm4249 <wmars503@gmail.com>
2022-11-03 11:05:46 -0700

    Test commit

--- README.md
+++ README.md
@@ -18,3 +18,8 @@
 
 ## Specifications
 
+Describe: beepBoop()
+
+Test: "It should return an array with a 0 if the number 0 is inputted"
+Code: beepBoop(0);
+Expected Output: [ 0 ]
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //Business Logic
 
-function userArray (numberStop) { 
+function beepBoop (numberStop) { 
     var numbers = [];
     for (let index = 0; index <= numberStop; index += 1) {
       var words = index.toString();
+    
+});
