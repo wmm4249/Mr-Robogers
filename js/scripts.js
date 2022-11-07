471701c35959c8453b9861b321611336e3812d90
wmm4249 <wmars503@gmail.com>
2022-11-07 09:13:33 -0800

    Add constants to logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 //Business Logic
+const beep = "Beep!"
+const boop = "boop!"
+const neighbor = "Won't you be my neighbor?"
 
 function beepBoop (input) { 
     var numbers = [];
     for (let i = 0; i <= input; i++) {
       var words = index.toString();
     
-});
+}};
