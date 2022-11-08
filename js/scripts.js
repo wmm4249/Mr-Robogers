c75319ee02ed9246a24b65a731474cf17f1f34f3
wmm4249 <wmars503@gmail.com>
2022-11-08 10:35:56 -0800

    Add console.log for debug purposes

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,22 @@ const boop = "boop!"
 const neighbor = "Won't you be my neighbor?"
 
 function beepBoop (input) { 
+  console.log(input+2)
     let numbers = [];
     for (let i = 0; i <= input; i++) {
       numbers.push(i,toString());
     };
   
     var robogers = numbers.map(function(number) {
+      console.log(typeof number)
       if (number.includes("1")) {
         return number = beep;
-        // } else if (number.includes("2")) {
-        //   return number = boop;
-        // } else if (number.includes("3")) {
-        //   return number = neighbor;
-      } else {
-        return number;
+        } else if (number.includes("2")) {
+          return number = boop;
+        } else if (number.includes("3")) {
+          return number = neighbor;
+        } else {
+          return number;
       };
     });
     return robogers;
@@ -30,7 +32,7 @@ $(document).ready(function() {
   $("form#number").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("input#input").val());
-    var result = robogers(input);
+    var result = beepBoop(input);
     $("#results").text(result);
     $(".output").show();
     });
